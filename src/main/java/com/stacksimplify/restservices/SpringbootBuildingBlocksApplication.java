02ad499611cc61f71c8005fcc872ff1a34175f9c
Raghavendra Jamili <raghavendrajamili@gmail.com>
2020-10-20 20:02:33 +0530

    Second Commit changed.

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingBlocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingBlocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildingBlocksApplication {
-
+//changed the comment
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingBlocksApplication.class, args);
 	}
